d3889cc2a88818d93849bbafbebd08f779ba2982
German Kröger <116314842+zaknein@users.noreply.github.com>
2025-09-19 19:10:54 -0300

    import random & scanner module

--- App.java
+++ App.java
@@ -1,7 +1,16 @@
+import java.util.Random;
+import java.util.Scanner;
 
 public class App{
 
     public static void main(String[] args) {
+
+        Random generator = new Random();
+        Scanner teclado = new Scanner(System.in);
+        int numeroAleatorio = generator.nextInt(101);
+
+        System.out.println(numeroAleatorio);
         
+
     }
 }

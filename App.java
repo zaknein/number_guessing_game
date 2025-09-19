a37f7a18e18eaf73a20a85a15fa8be3f2ebe925e
German Kröger <116314842+zaknein@users.noreply.github.com>
2025-09-19 19:20:52 -0300

    little mnenu

--- App.java
+++ App.java
@@ -10,7 +10,39 @@ public class App{
         int numeroAleatorio = generator.nextInt(101);
 
         System.out.println(numeroAleatorio);
+
+        System.out.println("Welcome to the Number Guessing Game!\r\n" + //
+                        "I'm thinking of a number between 1 and 100.\r\n" + //
+                        "You have 5 chances to guess the correct number.\r\n");
         
+        
+        boolean answer = false;
+        
+        while (!answer) {
+            System.out.println("Please select the difficulty level:\r\n" + //
+                        "1. Easy (10 chances)\r\n" + //
+                        "2. Medium (5 chances)\r\n" + //
+                        "3. Hard (3 chances)");
+
+            int selectedLevel = teclado.nextInt();
+            if (selectedLevel == 1 || selectedLevel == 2 || selectedLevel ==3) {
+                answer = true;
+                switch (selectedLevel) {
+                    case 1:
+                        System.out.println("Great! You have selected the Easy difficulty level.");
+                        break;
+                    case 2:
+                        System.out.println("Great! You have selected the Medium difficulty level.");
+                        break;
+                    case 3:
+                        System.out.println("Great! You have selected the Hard difficulty level.");
+                        break;                                           
+             
+                }
+            }
+        }
+
+
 
     }
 }
